e3dd1ad7fe6895abc086f5c029111f1df266c0f4
ngasaraphael <66059003+ngasaraphael@users.noreply.github.com>
2021-08-16 18:34:43 +0200

    task2.5 screenshots included

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 const port = 8080;
 
 let topMovies = [
@@ -10,7 +12,6 @@ let topMovies = [
     director: 'Frank Darabont',
     duration: '2h 22min',
     genre: ['Crime', 'Drama'],
-    score: 9.3,
   },
   {
     title: 'The Godfather',
@@ -18,7 +19,6 @@ let topMovies = [
     director: 'Francis Ford Coppola',
     duration: '2h 55min',
     genre: ['Crime', 'Drama'],
-    score: 9.2,
   },
   {
     title: 'The Godfather: Part II',
@@ -26,7 +26,6 @@ let topMovies = [
     director: 'Francis Ford Coppola',
     duration: '3h 22min',
     genre: ['Crime', 'Drama'],
-    score: 9,
   },
   {
     title: 'The Dark Knight',
@@ -34,7 +33,6 @@ let topMovies = [
     director: 'Christopher Nolan',
     duration: '2h 32min',
     genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-    score: 9,
   },
   {
     title: '12 Angry Men',
@@ -42,7 +40,6 @@ let topMovies = [
     director: 'Sidney Lumet',
     duration: '1h 36min',
     genre: ['Crime', 'Drama'],
-    score: 8.9,
   },
   {
     title: 'Schindler"s List',
@@ -50,7 +47,6 @@ let topMovies = [
     director: 'Steven Spielberg',
     duration: '3h 15min',
     genre: ['Biography', 'Drama', 'History'],
-    score: 8.9,
   },
   {
     title: 'Pulp Fiction',
@@ -58,7 +54,6 @@ let topMovies = [
     director: 'Quentin Tarantino',
     duration: '2h 34min',
     genre: ['Crime', 'Drama'],
-    score: 8.9,
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
@@ -66,7 +61,6 @@ let topMovies = [
     director: 'Peter Jackson',
     duration: '3h 21min',
     genre: ['Adventure', 'Drama', 'Fantasy'],
-    score: 8.9,
   },
   {
     title: 'Il buono, il brutto, il cattivo',
@@ -74,7 +68,6 @@ let topMovies = [
     director: 'Sergio Leone',
     duration: '3h 2min',
     genre: ['Western'],
-    score: 8.9,
   },
   {
     title: 'Fight Club',
@@ -82,25 +75,66 @@ let topMovies = [
     director: 'David Fincher',
     duration: '2h 19min',
     genre: ['Drama'],
-    score: 8.8,
   },
 ];
+
+//bodyParser Middleware for req.param.body
+app.use(bodyParser.json());
 
 //morgan middleware to log details
 app.use(morgan('common'));
 
-//movie route
+//static routes
+app.use(express.static('public'));
+
+//All movies route
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
-//title route
-app.get('/title', (req, res) => {
-  res.send('this is the title page');
+//route to Data about single movie route
+app.get('/movies/:title', (req, res) => {
+  res.send(
+    topMovies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
 });
 
-//static routes
-app.use(express.static('public'));
+//Route to Data about Genre
+app.get('/genres/:name', (req, res) => {
+  res.send('<h1>This is the genre Route </h1>');
+});
+
+//Route to Data about Director
+app.get('/directors/:name', (req, res) => {
+  res.send('<h1>This is the info Route about Director </h1>');
+});
+
+//Route to register new users
+app.post('/users', (req, res) => {
+  res.send('<h1>This is the Route to Register new Users</h1>');
+});
+
+//Route to Update users info
+app.put('/users/:username', (req, res) => {
+  res.send('<h1>This is the Route to Update user info</h1>');
+});
+
+//Route for users to add movies to favorite list
+app.post('/users/:username/:favorites/:movieID', (req, res) => {
+  res.send('<h1>This is the Route to add movie to favorite list</h1>');
+});
+
+//Route for users to remove movies from favorite list
+app.delete('/users/:username/:favorites/:movieID', (req, res) => {
+  res.send('<h1>This is the Route to remove movie from favorite list</h1>');
+});
+
+//Route to delete user
+app.delete('/users/:username', (req, res) => {
+  res.send('<h1>This is the Route to delete user</h1>');
+});
 
 //Error handling
 app.use((err, req, res, next) => {
