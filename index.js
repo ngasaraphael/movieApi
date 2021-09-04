75f9c5067bc18dcbb520319a99661e90e85457d5
ngasaraphael <66059003+ngasaraphael@users.noreply.github.com>
2021-09-04 02:10:58 +0200

    commenting out auth for testing

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 //implemeting cors
 //Allowed Origins by Cors//
-let allowedOrigins = [
-  'http://localhost:8081',
-  'https://nameless-retreat-07686.herokuapp.com/',
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// let allowedOrigins = [
+//   'http://localhost:8081',
+//   'https://nameless-retreat-07686.herokuapp.com/',
+// ];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
